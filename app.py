29b6c0dc7a3e81f7c70e80759b9c0077b3127c1b
Anu Kurien <anumarykurien@icloud.com>
2020-07-01 15:24:41 -0500

    Set up flask app: Conatct and stories route added

--- app.py
+++ app.py
@@ -9,4 +9,12 @@ def home():
 @app.route('/bio')
 def bio():
     return render_template('bio.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/stories')
+def stories():
+    return render_template('stories.html')
     
